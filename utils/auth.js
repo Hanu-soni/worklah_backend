744126d35ee55d2ba2375f328867cf2231815a9a
pankaj1155 <pankajsoni93444@gmail.com>
2025-06-26 15:02:31 +0530

    ,,,

--- utils/auth.js
+++ utils/auth.js
@@ -10,7 +10,7 @@ const setCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
     sameSite: 'lax',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 24 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
 
