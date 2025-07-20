f9db3fd1150e839bef4277be5b21dc7a420f37f6
pankaj1155 <pankajsoni93444@gmail.com>
2025-07-20 16:27:09 +0530

    updated changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -132,6 +132,7 @@ exports.generateOtp = async (req, res) => {
     // Send OTP using Twilio
     const otpStatus = await sendOTP(phoneNumber);
     console.log(otpStatus !== 'pending',"....otpStatusses")
+    console.log("otpStatus135", otpStatus)
 
     if (otpStatus !== 'pending'||otpStatus !== 'queued') {
       return res.status(500).json({ message: 'Failed to send OTP. Try again later.check///' });
